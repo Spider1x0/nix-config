8dc1907b461e671ec7f63598437257fca359413c
Spider0x1 <SamiFallatah@tutanota.com>
2023-12-16 23:30:12 +0300

    Edited the boot module and added a new service

---
+++ modules/nixos/services/syncthing/default.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+
+  cfg = config.wildgoo.services.syncthing;
+
+  in
+
+{
+  options.wildgoo.services.syncthing = {
+    enable = mkEnableOption "Syncthing file sharing server";
+  };
+  config = mkIf cfg.enable {
+    services.syncthing ={
+      enable = true;
+    };
+  };
+}
